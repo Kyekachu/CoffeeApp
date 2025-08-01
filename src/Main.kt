4680c364899d0502771db23bad801ad80a3afcb0
Kyekachu <kyrene-taaffe@hotmail.com>
2025-08-01 10:20:14 +0800

    o/

--- src/Main.kt
+++ src/Main.kt
@@ -2,15 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun CalculateCaffeineLevel() {
     print("how many hours have you worked?\n")
-    val HoursWorked = readln().toInt();
+    val HoursWorked = readln().toInt()
 
     print("How many bugs have you found?\n")
-    val BugsFound = readln().toInt();
+    val BugsFound = readln().toInt()
 
     print("What is your energy level?\n")
-    val EnergyLevel = readln();
+    val EnergyLevel = readln()
 
-    var EnergyBonus: Int = 0;
+    var EnergyBonus: Int = 0
 
     if (EnergyLevel == "Tired") {
         EnergyBonus = 5
@@ -30,10 +30,29 @@ fun CalculateCaffeineLevel() {
 
     var CaffieneLevelNum: Int = BugsFound * 2 + HoursWorked + EnergyBonus;
 
+    var CaffieneLevel: String = "none"
 
+    if (CaffieneLevelNum in 0..5) {
+        CaffieneLevel = "Light"
+    }
 
-    print("")
+    else if (CaffieneLevelNum in 6..10) {
+        CaffieneLevel = "Medium"
+    }
 
+    else if (CaffieneLevelNum in 11..15) {
+        CaffieneLevel = "Strong"
+    }
+
+    else if (CaffieneLevelNum >= 16) {
+        CaffieneLevel = "Rocket Fuel"
+    }
+
+    else {
+        CaffieneLevel = "Rocket Fuel"
+    }
+
+    print("The level of Caffiene you need is $CaffieneLevel!")
 }
 
 fun main() {
